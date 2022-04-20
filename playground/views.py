d42577668cdfea38c11b3102049ba6ef35ae0cd7
Ben Merchant <benjaminmerchant@gmail.com>
2022-04-20 00:08:29 -0400

    debugging tools

--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def calculate():
+    x = 11
+    y = 42
+    return x
+
+
 def say_hello(request):
+    x = calculate
+    y = 2
     return render(request, 'hello.html', {'name': 'Benjo'})
     # return HttpResponse('Heyooooo World')
